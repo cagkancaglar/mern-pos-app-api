dbaef93f73f82a765398cabde7632c7639c856ef
Çağkan Çağlar <cagkancaglar@hotmail.com>
2023-07-16 18:23:34 +0300

    crud-for-category

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const cors = require("cors");
 const dotenv = require("dotenv");
 const port = 5000;
 
 dotenv.config();
+
+const categoryRoute = require("./routes/categories");
 
 const connect = async () => {
   try {
@@ -15,9 +18,11 @@ const connect = async () => {
   }
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/categories", categoryRoute);
 
 app.listen(port, () => {
   connect();
