6b575ae20afd71a4ef475edd8a0f65b8dafbcda2
Çağkan Çağlar <cagkancaglar@hotmail.com>
2023-07-16 20:10:24 +0300

    crud-for-invoice

---
+++ routes/invoices.js
@@ -0,0 +1,24 @@
+const Invoice = require("../models/Invoice.js");
+const express = require("express");
+const router = express.Router();
+
+router.get("/get-all", async (req, res) => {
+  try {
+    const invoices = await Invoice.find();
+    res.status(200).json(invoices);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.post("/add-invoice", async (req, res) => {
+  try {
+    const newInvoice = new Invoice(req.body);
+    await newInvoice.save();
+    res.status(200).json("Item added successfully.");
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ dotenv.config();
 
 const categoryRoute = require("./routes/categories");
 const productRoute = require("./routes/products");
+const invoiceRoute = require("./routes/invoices");
 
 const connect = async () => {
   try {
@@ -25,6 +26,7 @@ app.use(cors());
 
 app.use("/api/categories", categoryRoute);
 app.use("/api/products", productRoute);
+app.use("/api/invoices", invoiceRoute);
 
 app.listen(port, () => {
   connect();
